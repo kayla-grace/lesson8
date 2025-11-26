c652b5b45f60e836bccc11aa4d23772082f9053a
kayla-grace <heilingkayla@gmail.com>
2025-11-26 10:23:47 -0600

    fixed test cases and code coverage

--- _test_/appOperations.test.js
+++ _test_/appOperations.test.js
@@ -1,5 +1,9 @@
 const appOperations = require('../src/appOperations');
 
 test("multiplcation property of zero",()=>{
-    expect(appOperations(5,0)).toBe(0);
-})
+    expect(appOperations.multipy(5,0)).toBe(0);
+});
+
+test("adding two values",()=>{
+    expect(appOperations.add(5,5)).toBe(10);
+});
